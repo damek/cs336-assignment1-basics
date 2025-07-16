a98ee0341d06e93c2c2bd1f7ce668298814bcdec
damek <damek@users.noreply.github.com>
2025-07-16 19:16:15 -0400

    fix: update checkpoint argument handling in sampler.py to use pathlib for improved path management

--- cs336_basics/sampler.py
+++ cs336_basics/sampler.py
@@ -1,6 +1,6 @@
 import torch
 import configs, tokenizer_utils, transformer, optimization
-import argparse
+import argparse, pathlib
 
 # Get command line arguments from user 
 ## Checkpoint location 
@@ -22,8 +22,7 @@ p.add_argument("--num_tokens_per_sample", type=int, default=1)
 args = p.parse_args()
 
 ## Load model
-print(args.checkpoint)
-model, _ = optimization.load_checkpoint(args.checkpoint, model=None, optimizer=None)
+model, _ = optimization.load_checkpoint(pathlib.Path(args.checkpoint), model=None, optimizer=None)
 
 ## Load tokenizer
 tokenizer = tokenizer_utils.Tokenizer.from_files(args.vocab_path, args.merges_path)
