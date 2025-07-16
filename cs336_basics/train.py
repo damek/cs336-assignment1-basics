24c1743fcf65fd15cc45ff98a0015aa90089f7da
damek <damek@users.noreply.github.com>
2025-07-16 12:49:48 -0400

    fix: correct variable name for current iteration in cosine learning rate scheduler in train.py

--- cs336_basics/train.py
+++ cs336_basics/train.py
@@ -78,7 +78,7 @@ def main():
         ema_loss = 0
     if args.lr_scheduler == "cosine":
         print("Using cosine learning rate scheduler")
-        optimizer.lr = optimization.learning_rate_schedule(iter, args.cosine_decay["max_lr"], args.cosine_decay["min_lr"], args.cosine_decay["warmup_steps"], args.cosine_decay["cosine_cycle_iters"])
+        optimizer.lr = optimization.learning_rate_schedule(current_iter, args.cosine_decay["max_lr"], args.cosine_decay["min_lr"], args.cosine_decay["warmup_steps"], args.cosine_decay["cosine_cycle_iters"])
     else: 
         optimizer.lr = args.lr
 
