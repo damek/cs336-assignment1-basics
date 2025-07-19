93017644892fe66bd867df6cbb2740f52cba108c
damek <damek@users.noreply.github.com>
2025-07-18 22:32:56 -0400

    fix: replace x.rsqrt with torch.rsqrt in RMSNorm normalization for consistency and performance improvement

--- cs336_basics/transformer.py
+++ cs336_basics/transformer.py
@@ -38,7 +38,7 @@ class RMSNorm(nn.Module):
         x = x.to(dtype=torch.float32)
         d_model = self.d_model
         norm = x.sqrt().mean(dim=-1)
-        result = (x*x.rsqrt(norm + self.eps))*self.param
+        result = (x*torch.rsqrt(norm + self.eps))*self.param
         # denoms = torch.sqrt(einsum(x.square(), "... d_model -> ... ")/d_model + self.eps)
         # result = einsum(x*self.param, 1./denoms , "... d_model, ... -> ... d_model")
         return result.to(in_dtype)
