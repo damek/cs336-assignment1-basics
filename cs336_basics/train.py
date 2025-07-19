68b5e87566fb501f623684f0bd2b5d4004891446
damek <damek@users.noreply.github.com>
2025-07-19 14:56:12 -0400

    refactor: update optimizer zero_grad call in train.py to set_to_none for improved memory efficiency

--- cs336_basics/train.py
+++ cs336_basics/train.py
@@ -78,7 +78,7 @@ def main():
     validation = torch.as_tensor(np.memmap(args.val_data,dtype=np.uint16,mode="r"), dtype=torch.long, device=args.device)
     args.valid_size = validation.size
     windowed_validation = validation.unfold(0, args.context_length + 1, args.context_length)
-    windowed_validation = windowed_validation.pin_memory() if args.device == "cuda" else windowed_validation
+
     if args.device == "cuda" and torch.cuda.is_available():
         args.device = "cuda"
     elif args.device == "mps" and torch.backends.mps.is_available():
@@ -155,7 +155,7 @@ def main():
         if args.lr_scheduler == "cosine":
             optimizer.set_lr(optimization.learning_rate_schedule(iter, args.cosine_decay["max_lr"], args.cosine_decay["min_lr"], args.cosine_decay["warmup_steps"], args.cosine_decay["cosine_cycle_final_iter"]))
 
-        optimizer.zero_grad()
+        optimizer.zero_grad(set_to_none=True)
         loss = training_step(model, data, targets)
         if args.grad_clip is not None:
             optimization.gradient_clipping(model.parameters(), args.grad_clip)
