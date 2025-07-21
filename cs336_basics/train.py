b51b6cfad6b19aaf0ec8fa2ab456387bec3da0cc
damek <damek@users.noreply.github.com>
2025-07-21 14:44:49 -0400

    refactor: update optimizer parameter groups in training step of transformer_lm to improve weight decay handling

--- cs336_basics/train.py
+++ cs336_basics/train.py
@@ -231,7 +231,7 @@ def main():
         }
     ]
 
-    optimizer = optimization.AdamW(model.parameters(), betas = args.betas, eps = args.eps, weight_decay=args.weight_decay)
+    optimizer = optimization.AdamW(param_groups, betas = args.betas, eps = args.eps, weight_decay=args.weight_decay)
     # optimizer = torch.optim.SGD(model.parameters(), weight_decay=args.weight_decay, momentum=.95)
     print("Weight decay", args.weight_decay)
     current_iter = 0
