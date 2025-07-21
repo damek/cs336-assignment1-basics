3418bfc6a66a5e4b3b2d049a2fa5d6d8deb8dffa
damek <damek@users.noreply.github.com>
2025-07-21 15:31:46 -0400

    refactor: correct learning rate adjustment for output layer in training step of transformer_lm by multiplying with d_model

--- cs336_basics/train.py
+++ cs336_basics/train.py
@@ -272,7 +272,7 @@ def main():
             for group in optimizer.param_groups:
                 group["lr"] = optimization.learning_rate_schedule(iter, args.cosine_decay["max_lr"], args.cosine_decay["min_lr"], args.cosine_decay["warmup_steps"], args.cosine_decay["cosine_cycle_final_iter"])
             with torch.no_grad():
-                optimizer.param_groups[0]["lr"] = optimizer.param_groups[0]["lr"]/args.d_model
+                optimizer.param_groups[0]["lr"] = optimizer.param_groups[0]["lr"]*args.d_model
 
         optimizer.zero_grad()
         loss = training_step(model, data, targets)
